3d152bc8fdec10cf3734f94cd50bb31cb8233448
harsh <hsrivastava389@gmail.com>
2026-09-18 16:51:40 +0530

    chnage url in client api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+export const API_URL = "https://chatroom-6dks.onrender.com/api";
+export const SOCKET_URL = "https://chatroom-6dks.onrender.com";
 
 const api = axios.create({ baseURL: API_URL });
 
